7fc4510061c41a4033a0c743958642f7c23a7c0e
aleph ramos <105456824+alephsramos@users.noreply.github.com>
2024-09-28 12:36:03 -0300

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// Renderiza o App dentro do div com id "root" no index.html
-ReactDOM.render(<App />, document.getElementById('root'));
